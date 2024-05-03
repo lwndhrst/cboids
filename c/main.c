d8928d36661a191d08f2c8d8f713063e62d255f6
Leon Windhorst <leon@windhorst.dev>
2024-05-03 17:32:50 +0200

    Begin implementation in C

--- c/main.c
+++ c/main.c
@@ -1,23 +1,64 @@
 #include "raylib.h"
 
+#include <stdlib.h>
+
 typedef struct {
-    float turn_factor;
-    float visual_range;
-    float protected_range;
-    float centering_factor;
-    float avoid_factor;
-    float matching_factor;
-    float max_speed;
-    float min_speed;
-    float max_bias;
-    float bias_increment;
-    float bias_val;
+    double turn_factor;
+    double visual_range;
+    double protected_range;
+    double centering_factor;
+    double avoid_factor;
+    double matching_factor;
+    double max_speed;
+    double min_speed;
+    double max_bias;
+    double bias_increment;
+    double bias_val;
 } Params;
+
+typedef struct {
+    double x, y, dx, dy;
+} Boid;
+
+const int screen_width = 800;
+const int screen_height = 450;
+const int screen_margin = 50;
+
+void
+init_boids(Boid *boids, int num_boids, Params *params)
+{
+    for (int i = 0; i < num_boids; ++i)
+    {
+        Boid *boid = &boids[i];
+        boid->x = (double)GetRandomValue(0, screen_width);
+        boid->y = (double)GetRandomValue(0, screen_height);
+        boid->dx = 0.0f;
+        boid->dy = 0.0f;
+    }
+}
+
+void
+run_simulation(Boid *boids, int num_boids)
+{
+}
+
+void
+draw_boids(Boid *boids, int num_boids)
+{
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    for (int i = 0; i < num_boids; ++i)
+    {
+        Boid *boid = &boids[i];
+        DrawCircle(boid->x, boid->y, 5.0f, PURPLE);
+    }
+    EndDrawing();
+}
 
 int
 main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(screen_width, screen_height, "raylib [core] example - basic window");
 
     Params params = {
         0.2f,     // turn_factor
@@ -33,13 +74,17 @@ main(void)
         0.001f,   // bias_val
     };
 
+    const int num_boids = 100;
+    Boid *boids = malloc(num_boids * sizeof(Boid));
+    init_boids(boids, num_boids, &params);
+
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
+        run_simulation(boids, num_boids);
+        draw_boids(boids, num_boids);
     }
+
+    free(boids);
 
     CloseWindow();
 
