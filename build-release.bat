7910934868b9840c8423c168acab135935d4b227
Leon Windhorst <leon@windhorst.dev>
2024-05-21 08:28:03 +0200

    Update CMakeLists.txt

--- build-release.bat
+++ build-release.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cmake -S . -B build/release -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE
+cmake -S . -B build/release -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build build/release
